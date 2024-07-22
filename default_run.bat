53d72bba68f984fd82582569b1494887ef0b0f23
oitsjustjose <stovallj1995@gmail.com>
2024-07-21 19:13:45 -0500

    Include default run.bat for user reference

---
+++ default_run.bat
@@ -0,0 +1,7 @@
+start /b \
+    <PATH_TO dirbackup.exe> \
+    -s <PATH_TO_DIR_TO_BACKUP> \
+    -d <PATH_TO_BACKUP_DESTINATION> \
+    -i (OPTIONAL) <IGNORE_REGEX_PATH> \
+    -o (OPTIONAL) <PATH_TO_LOGs_DIR> \
+    -l (OPTIONAL) <LOG_LEVEL>
